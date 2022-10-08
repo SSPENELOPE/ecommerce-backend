2a56be7092f608c0e1ad8e98c27a82808914d907
SSPENELOPE <typoepping@gmail.com>
2022-10-08 18:37:38 -0400

    added all product routes and get routes for tags

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -16,7 +16,7 @@ Category.hasMany(Product, {
 })
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsTo(ProductTag, {
+Product.hasMany(ProductTag, {
   foreignKey: 'product_id',
 })
 
