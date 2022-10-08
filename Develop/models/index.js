7021a0e3ba59bda2763fc137883919de3cd9df0f
SSPENELOPE <typoepping@gmail.com>
2022-10-08 15:18:03 -0400

    added content to model files

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'cascade',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsTo(ProductTag, {
+  foreignKey: 'product_id',
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsTo(ProductTag, {
+  foreignKey: 'tag_id',
+});
 
+// Export the models
 module.exports = {
   Product,
   Category,
