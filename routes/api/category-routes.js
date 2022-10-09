17e8a980bdcd8229a8cb7a1b0a83b7ee6c59f262
SSPENELOPE <typoepping@gmail.com>
2022-10-09 15:49:35 -0400

    updated package.json, changed route paths to match directory

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { json } = require('sequelize');
-const { Category, Product } = require('../../../models');
+const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
